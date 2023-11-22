969f2b144b504fe627680e59a1cee24b7525d2d2
Nithin <badinithin@gmail.com>
2023-11-22 15:27:07 +0530

    Made changes

--- app.js
+++ app.js
@@ -4,10 +4,25 @@ var search=[];
 const prompt='in';
 const container = document.querySelector('.container')
 function createCard(c){
+    
     const div = document.createElement('div');
     const img = document.createElement('img');
-    img.src=c.flags.svg
+    const name = document.createElement('span');
+    const pop = document.createElement('span');
+    const region = document.createElement('span');
+    const capital = document.createElement('span');
+    name.innerHTML = `<b>${c.name}</b>`;
+    name.classList.add('name');
+    img.src=c.flags.svg;
+    pop.innerHTML = `<b>Population:</b> ${c.population}`;
+    region.innerHTML = `<b>Region:</b> ${c.region}`;
+    capital.innerHTML = `<b>Capital:</b> ${c.capital}`;
+
     div.appendChild(img);
+    div.appendChild(name);
+    div.appendChild(pop);
+    div.appendChild(region);
+    div.appendChild(capital);
     div.classList.add('card');
     container.appendChild(div);
 }
@@ -26,14 +41,13 @@ for(let i=0;i<act.length;i++){
 ['input','change'].forEach(elem=>{
     cont.addEventListener(elem,(e)=>{
         console.clear();
+        container.innerHTML =``;
         for(let s of search){
             if(s.toLowerCase().indexOf(promptInput.value) == 0){
                 var d = act[search.indexOf(s)]
                 if(d.region===filter.value){
-                    
                     createCard(d);
                 }else if(filter.value==='all'){
-                    
                     createCard(d);
                 }
             }
